cc51310e9783c98388fa4196e9ec717c02d2aaf7
TonicB <a.sharafiev@gmail.com>
2023-05-23 22:17:31 +0300

    feat: string-task resolved

---
+++ src/test.js
@@ -0,0 +1,11 @@
+function encodeToRot13(str) {
+  const template = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+  const templateUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return str.replace(/[A-Z|a-z]/g, (x) => {
+    if (/[A-Z]/.test(x)) {
+      return templateUp.at(templateUp.indexOf(x) + 13);
+    }
+    return template.at(template.indexOf(x) + 13);
+  });
+}
+console.log(encodeToRot13('Why did the chicken cross the road?'));
